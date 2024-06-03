76ab467e41c3094d6ecf823ab54e31688d4963d9
Doan Thanh Luc <lucdt@solazu.com>
2024-06-03 10:14:30 +0700

    add middleware remove NoXPowered

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { DatabaseModule } from '@database/database.module';
+import { NoXPoweredByMiddleware } from '@middlewares/no-x-powered-by.middleware';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(NoXPoweredByMiddleware).forRoutes('*');
+  }
+}
