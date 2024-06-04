ae862bf2bdc8c652571c179718ad7f38448af98b
Doan Thanh Luc <lucdt@solazu.com>
2024-06-04 16:14:46 +0700

    fix bash script run source on server

--- bash-setup-server.sh
+++ bash-setup-server.sh
@@ -2,12 +2,7 @@ clear
 echo "Script by https://www.facebook.com/doanthanhluc300420"
 echo "Support: Ubuntu/Centos"
 echo "Đang nhận diện distro linux"
-echo "Vui lòng port để mở cho server (mặc định port 80):"
-PORT_DEFAULT=80
-read -p "" PORT
-if [ -z "$PORT" ]; then
-    PORT="$DEFAULT_VALUE"
-fi
+PORT_SERVER=5000
 
 dist=$(hostnamectl | egrep "Operating System" | cut -f2 -d":" | cut -f2 -d " ")
 if [ $dist = "CentOS" ] ; then
@@ -38,16 +33,22 @@ elif [ $dist = "Ubuntu" -o $dist = "Debian" ] ; then
 	echo "Đang cài đặt phần mềm cần thiết"
 	sudo apt install -y docker docker.io
         clear
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        clear
+        sudo chmod +x /usr/local/bin/docker-compose
+        clear
+        docker-compose --version
+        clear
 	echo "Đang mở port"
-        sudo ufw allow $PORT
+        sudo ufw allow $PORT_SERVER
         clear
 fi
 echo "Đang khởi tạo server LT_HOME"
 sudo docker-compose up -d
 clear
-IP=$(curl -s ifconfig.me)
+# IP=$(curl -s ifconfig.me)
 echo "Đang kiểm tra kết nối tới LT_HOME server"
 sleep 2
-echo -n "- Đang khởi tạo kết nối: " && if curl -s --connect-timeout 1 $IP | grep -q Bad; then echo -e "$(tput setaf 2)ONLINE - Ping thành công đến IP$(tput sgr0)"; else echo -e "$(tput setaf 1)OFFLINE - Không thể Ping đến IP, vui lòng mở port 80$(tput sgr0)"; fi
-echo "Address (Địa chỉ): $IP"
-echo "Port (Cổng): $PORT"
+PUBLIC_IP=$(curl -s https://icanhazip.com)
+echo "Address (Địa chỉ): $PUBLIC_IP"
+echo "Port (Cổng): $PORT_SERVER"
