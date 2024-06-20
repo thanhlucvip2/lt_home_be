7e7860fcd87d74438a6035037901ea7958f23a78
Doan Thanh Luc <lucdt@solazu.com>
2024-06-20 16:55:13 +0700

    update drawio diagram

--- src/migrations/1712128860745-create-user-table.ts
+++ src/migrations/1712128860745-create-user-table.ts
@@ -1,13 +1,17 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateInventoryTable1712128860745 implements MigrationInterface {
-  name = 'CreateInventoryTable1712128860745';
+export class CreateUserTable1712128860745 implements MigrationInterface {
+  name = 'CreateUserTable1712128860745';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      CREATE TABLE inventory (
+      CREATE TABLE user (
         id INT PRIMARY KEY AUTO_INCREMENT,
-        
+        first_name VARCHAR(255),
+        last_name VARCHAR(255),
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        role INT DEFAULT 1,
         create_at DATETIME DEFAULT CURRENT_TIMESTAMP,
         update_at DATETIME DEFAULT CURRENT_TIMESTAMP,
         CONSTRAINT email_unique UNIQUE (email)
@@ -16,6 +20,6 @@ export class CreateInventoryTable1712128860745 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE IF EXISTS inventory`);
+    await queryRunner.query(`DROP TABLE IF EXISTS user`);
   }
 }
