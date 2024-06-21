158c8288e6e11978b52e787db7829b282e2500bd
Doan Thanh Luc <lucdt@solazu.com>
2024-06-21 17:58:40 +0700

    products dto

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,3 +1,11 @@
+import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { assign } from 'lodash';
 import { API_PREFIX_PATH } from '@configs/app.config';
 import { ResponseModel } from '@model/response.model';
 import { ServiceGuard } from '@modules/auth/guards/guards.service';
@@ -6,23 +14,19 @@ import {
   Get,
   HttpException,
   HttpStatus,
+  Post,
   Req,
   Res,
   UseGuards,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import {
-  ApiBadRequestResponse,
-  ApiBearerAuth,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
 import { AppRequests, AppResponse } from '@model/index.model';
 import { UserModel } from '@model/user.model';
+import { ProductsService } from './products.service';
 
 @UseGuards(AuthGuard('jwt'), ServiceGuard)
 @Controller(`${API_PREFIX_PATH}/products`)
 export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
   @ApiBearerAuth('token')
   @ApiResponse({ description: 'get-products-success' })
   @ApiBadRequestResponse({ description: 'Unauthorized' })
@@ -38,6 +42,32 @@ export class ProductsController {
     const currentUser: UserModel = user;
     console.log(currentUser);
     try {
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    return res.status(HttpStatus.OK).json(resData);
+  }
+  @ApiBearerAuth('token')
+  @ApiResponse({ description: 'get-products-success' })
+  @ApiBadRequestResponse({ description: 'Unauthorized' })
+  @ApiTags('Products')
+  @Post()
+  async createProduct(@Res() res: AppResponse, @Req() req: AppRequests) {
+    const resData: ResponseModel<any> = {
+      statusCode: HttpStatus.OK,
+      success: 'create-products-success',
+      data: null,
+    };
+
+    try {
+      const { user } = req;
+      const currentUser: UserModel = user;
+      const data = await this.productsService.create({
+        user: currentUser,
+      });
+      assign(resData, {
+        data,
+      });
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ProductsRepository } from './products.repository';
+import { UserModel } from '@model/user.model';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(private readonly productsRepository: ProductsRepository) {}
+
+  async create(payload: { user: UserModel }) {
+    return payload;
+  }
+}
